ba5c02050a79510e68777b330d8b45e0cebcd019
Gautam Adatra <gautamkiran123@gmail.com>
2023-06-04 12:05:30 +0530

    error handling

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -16,18 +16,18 @@ export const ExpenseItem = ({expense, showBudget}) => {
 
     return(
         <>
-        <td> {expense.name} </td>
-        <td>{formatCurrency(expense.amount)}</td>
-        <td>{formatDateToLocaleString(expense.createdAt)}</td>
+        <td> {expense?.name} </td>
+        <td>{formatCurrency(expense?.amount)}</td>
+        <td>{formatDateToLocaleString(expense?.createdAt)}</td>
         {
             showBudget && (
-                <td> <Link to={`/budget/${budget.id}`}>{budget.name}</Link> </td>
+                <td> <Link to={`/budget/${budget?.id}`}>{budget?.name}</Link> </td>
             )
         }
         <td>
             <fetcher.Form method="post">
                 <input type="hidden" name="_action" value="deleteExpense" />
-                <input type="hidden" name="expenseId" value={expense.id} />
+                <input type="hidden" name="expenseId" value={expense?.id} />
                 <button type="submit" name="expenseId">
                     Delete
                 </button>
