69a0bfc2c63bde93f4b09b5e6f22cb92073b8aeb
Surbhi Pandey <khushipandey984@gmail.com>
2023-06-05 16:28:37 +0530

    Trash-icon

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { formatCurrency } from "../utils/format-currency";
 import { getAllItems } from "../utils/get-all-items";
 import { Link, useFetcher } from "react-router-dom";
+import { FaTrash } from 'react-icons/fa';
+import styled from "styled-components";
 
 export const ExpenseItem = ({expense, showBudget}) => {
     const fetcher = useFetcher();
@@ -28,12 +30,17 @@ export const ExpenseItem = ({expense, showBudget}) => {
             <fetcher.Form method="post">
                 <input type="hidden" name="_action" value="deleteExpense" />
                 <input type="hidden" name="expenseId" value={expense.id} />
-                <button type="submit" name="expenseId">
-                    Delete
-                </button>
+                <Button type="submit" name="expenseId">
+                    <FaTrash color="red"/>
+                </Button>
 
             </fetcher.Form>
         </td>
         </>
     )
 }
+
+const Button = styled.button`
+    border: none;
+    background: none;
+`
